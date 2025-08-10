61cdadee2d0fcc4bd299a943192fcfbe7eb2b323
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-08-10 03:16:19 +0200

    chore: init

---
+++ next-sitemap.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://lorimerjenkins.com",
+  generateRobotsTxt: true,
+  changefreq: "daily",
+  priority: 1,
+  sitemapSize: 5000,
+  exclude: ["/"],
+  additionalPaths: async (config) => {
+    const basePaths = [
+      {
+        loc: "/about",
+        changefreq: "daily",
+        priority: 0.9,
+        lastmod: new Date().toISOString(),
+      },
+    ];
+    return [...basePaths];
+  },
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: new Date().toISOString(),
+    };
+  },
+};
