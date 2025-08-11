670a65d76a8e9deb1b3a63663aa760ae0949eb6f
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-08-11 07:20:35 +0200

    -

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,38 +1,7 @@
-import { Metadata } from "next";
-import dynamic from "next/dynamic";
-import { siteWideSEO } from "@/utils/SEO/SEO";
 import About from "./about";
+import { metadata as data } from "@/utils/SEO/SEO";
 
-const title = "Lorimer Jenkins - About";
-const url = "https://liquidops.io/markets";
-const imagePath = "https://liquidops.io/SEO/markets.png";
-
-export const metadata: Metadata = {
-  title,
-  description: siteWideSEO.marketDescription,
-  alternates: {
-    canonical: url,
-  },
-  openGraph: {
-    title,
-    description: siteWideSEO.marketDescription,
-    url,
-    images: [
-      {
-        url: imagePath,
-        width: 1200,
-        height: 630,
-        alt: title,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title,
-    description: siteWideSEO.marketDescription,
-    images: [imagePath],
-  },
-};
+export const metadata = data;
 
 const Page = () => {
   return <About />;
--- src/utils/SEO/SEO.ts
+++ src/utils/SEO/SEO.ts
@@ -1,16 +1,76 @@
 import { keyWords } from "./keywords";
-
-// SEO content
-const siteWideDescription = `Lorimer Jenkins is a tech entrepreneur from the UK, building LiquidOps a decentralized lending and borrowing protocol built in the Arweave and AO Web3 ecosystem.`;
+import { Metadata } from "next";
 
 // global SEO vars
-export const siteWideSEO = {
-  name: "LiquidOps",
-  url: "https://liquidops.io",
-  description: siteWideDescription,
+export const SEO = {
+  name: "Lorimer Jenkins",
+  url: "https://lorimerjenkins.com",
+  description: `Lorimer Jenkins is a tech entrepreneur from the UK, building LiquidOps a decentralized lending and borrowing protocol built in the Arweave and AO Web3 ecosystem.`,
   keyWords,
   icons: [
-    { url: "/favicon.ico", sizes: "any", type: "image/x-icon" },
-    { url: "/favicon.svg", type: "image/svg+xml" },
+    {
+      url: "https://arweave.net/bdL4InvwkBhT5gZ3dq-mhS3lzMCwTVcf-UxMivpKcRQ",
+      sizes: "any",
+      type: "image/x-icon",
+    },
+    {
+      url: "https://arweave.net/MjJ3aY7v6nafYduLtjZaPbh0Lf2OzEIgr_tDlzdmeas",
+      type: "image/svg+xml",
+    },
   ],
+  shareImagePath:
+    "https://arweave.net/Fqu7AZ6B5vkb9KLgQFfV_Yd0egSafsW39W8AIMRDu-Q",
+  socialMedia: [
+    "https://x.com/Lorimer_Jenkins",
+    "https://www.linkedin.com/in/lorimerjenkins",
+    "https://github.com/LorimerJenkins",
+  ],
+};
+
+export const metadata: Metadata = {
+  title: SEO.name,
+  description: SEO.description,
+  keywords: SEO.keyWords,
+  icons: SEO.icons,
+  openGraph: {
+    title: SEO.name,
+    description: SEO.description,
+    url: SEO.url,
+    siteName: SEO.name,
+    images: [
+      {
+        url: SEO.shareImagePath,
+        width: 1200,
+        height: 630,
+        alt: SEO.name,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  alternates: {
+    canonical: SEO.url,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: SEO.name,
+    description: SEO.description,
+    images: [SEO.shareImagePath],
+  },
+  robots: "index, follow",
+  other: {
+    "script:ld+json": JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "Person",
+      name: SEO.name,
+      description: SEO.description,
+      url: SEO.url,
+      jobTitle: "Co-founder",
+      worksFor: {
+        "@type": "Organization",
+        name: "LiquidOps",
+      },
+      sameAs: SEO.socialMedia,
+    }),
+  },
 };
