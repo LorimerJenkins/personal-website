d2d1033e77bdce6ab5c641a2b95bd131b425040a
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-08-12 01:24:25 +0200

    chore: multiple langauge support

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -2,147 +2,19 @@
 import styles from "./about.module.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import { useTranslations } from "next-intl";
 
 function About() {
+  const t = useTranslations("About");
+
   return (
     <div className={styles.page}>
       <Header />
       <div className={styles.body}>
-        <p className={styles.title}>Who is Lorimer Jenkins?</p>
-        <p>
-          Lorimer Jenkins is a tech entrepreneur from the UK, building LiquidOps
-          a decentralized lending and borrowing protocol built in the Arweave
-          and AO Web3 ecosystem.
-        </p>
-        <p className={styles.title}>About me</p>
-        <p>
-          I was born in the United Kingdom on the 30th of July 2003, I grew up
-          in Hertfordshire and was trained from a young age in Acting and
-          Musical Theatre.
-        </p>
-        <p>
-          Although I liked Acting, at age 17 I decided it was not for me and
-          decided to pursue other careers. The first place I started to look was
-          in trying to find something to do with my passion for something called
-          Blockchain, I had always longed to work for a Blockchain company but
-          never thought I was smart enough or had the degrees to do it.
-        </p>
-        <p>
-          Most of my peers were pursuing traditional academia at college/uni
-          studying STEM subjects, I felt I had shot myself in the foot as at the
-          time I was at a acting college. This feeling later turned into one of
-          my most valued strengths, being the underdog.
-        </p>
-        <p>
-          I remember watching a Gary Vee video at the time on him saying that he
-          worked for free when he was younger, as it is super de-risked for a
-          company to hire someone for free, they could take a bigger bet on who
-          they hired.
-        </p>
-        <p>
-          So I set out and emailed over 500 companies, founders and investment
-          firms. I think out of all the emails I did, only about 6 of them hit,
-          I am still close with all 6 and continue to work alongside them in
-          many ways to this day.
-        </p>
-        <p>
-          One of the 6 worked at a venture fund that invested in crypto, he saw
-          how hungry I was to just work and put me in contact with the MP of the
-          firm who was looking for more interns.
-        </p>
-        <p>
-          This started everything for me, I worked at the firm as a unpaid
-          intern for 3 months and after continued to work there as a paid
-          analyst for a year. Looking back it was the most mind numbing boring
-          work, but I absolutely loved it, I worked on their crypto VC and HF.
-        </p>
-        <p>
-          Having worked at the firm for a year, the partners begun to trust me
-          more and I started turning up to pitching calls to make notes for
-          their IC. I started trying to look for interesting companies for the
-          fund on X one day and I came across a guy called Tate building a DEX.
-        </p>
-        <p>
-          Up until this point I was always the youngest in the room by far, Tate
-          was my age (18) at the time and I was ecstatic to chat with him as the
-          founder of a DEX at MY AGE.
-        </p>
-        <p>
-          I spoke with Tate a few times about his project, sadly the fund did
-          not invest, but I stayed in contact with him.
-        </p>
-        <p>
-          Although working at a investment fund is great, it is a little boring
-          compared to building a startup. So one day I messaged Tate and
-          approached him saying that I was looking for a job and if he would be
-          interested in working together, he said no.
-        </p>
-        <p>
-          He said no as it was tough to see what role I would fit into,
-          extremely gutted and full of the underdog mentality I had developed I
-          taught myself to code in a few months.
-        </p>
-        <p>
-          This led me to build my first project, a analytics website for crypto
-          transactions. This project grew and we were given a grant for us (me
-          and a few Fiverr contractors) to focus on it full time. I quit my
-          investment job to work on the project full time and did for a year.
-        </p>
-        <p>
-          After some time I re approached Tate, showed him the project and told
-          him I learnt to code. I think this excited Tate as he met so many
-          people who said they were going to do things but never did.
-        </p>
-        <p>
-          Tate was working on a new idea at the time called Community Labs, they
-          had just raised their seed round. I took this opportunity with open
-          arms, returned the analytics grant we received and joined as the first
-          outside Fellow.
-        </p>
-        <p>
-          I worked at Community Labs for 2 years or so building various projects
-          like Othent, Oases, subsidise-JS and AO Ventures.
-        </p>
-        <p>
-          While in Community Labs I met Marton, we shared many similarities and
-          grew close in the company. Marton was the founder of a project called
-          ArConnect, a Web3 Arweave wallet.
-        </p>
-        <p>
-          One day while he was on a trip to Cambridge, where I was living at the
-          time, we realized how much potential there was for all these tokens in
-          Arweave DeFi.
-        </p>
-        <p>
-          We were super lucky that AO, a new computing layer on top of Arweave,
-          was in it's infancy. We came up with a idea of a DeFi protocol where
-          you could earn interest on your AR tokens and called it Operation
-          Liquidity (lets bring liquidity to Arweave).
-        </p>
-        <p>
-          We launched a waitlist where you could sign up with your Arweave
-          wallet and pledge assets to the protocol.
-        </p>
-        <p>
-          Within the first two hours that waitlist grew to over $2m and over the
-          next few months grew to $55m with over 3 thousand email signups.
-        </p>
-        <p>
-          We then re-named the idea to LiquidOps (Operation Liquidity backwards
-          and shortened), we raised a pre-seed round for idea of $325k and spun
-          out Community Labs as the first fellow spin outs (first one in first
-          one out haha).
-        </p>
-        <p>
-          I now spend my time working on LiquidOps, a lending and borrowing
-          protocol built on Arweave and AO.
-        </p>
-        <p>
-          Thanks for reading, I hope my story can inspire you to chase your
-          dreams from a young kid in his attic wanting to work at a Blockchain
-          company, to now being the Co-founder and CEO of one, we are just
-          getting started at LiquidOps and I am excited to keep building!
-        </p>
+        <p className={styles.title}>{t("title1")}</p>
+        <p>{t("paragraph1")}</p>
+        <p className={styles.title}>{t("title2")}</p>
+        <p style={{ whiteSpace: "pre-line" }}>{t("paragraph2")}</p>
       </div>
       <Footer />
     </div>
