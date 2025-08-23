bed988e18c3f70037c13735b389e34f61d82270e
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-08-23 15:47:49 +0200

    permaweb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { metadata as data } from "@/utils/SEO/SEO";
 import { QueryProvider } from "@/lib/queryClient";
+import Script from "next/script";
 
 export const metadata = data;
 
@@ -17,6 +18,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZSCZDNP5XG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZSCZDNP5XG');
+          `}
+        </Script>
+      </head>
       <body className={dmSans.className} style={{ margin: 0, padding: 0 }}>
         <QueryProvider>{children}</QueryProvider>
       </body>
