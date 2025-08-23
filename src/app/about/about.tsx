56dd84295c451ce9df113f9c075a015bbbd9c923
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-08-23 15:20:49 +0200

    permaweb

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -35,12 +35,3 @@ function About() {
 }
 
 export default About;
-// 🧵 1/3 I am finally put something on my lorimer.arweave.dev site (a big fat image of my face).
-
-// 👇 But also...
-
-// 2/3 The other day I was complaining about life, but realized everything I was complaining about would have been something I would of killed for in the past.
-
-// I am often told I have an inspiring story, so I have written it up from first begging work to now building @Liquid_Ops.
-
-// 3/3 👉 Read it at lorimer.arweave.dev/about, I hope it can inspire you to follow your dreams and if nothing else be an interesting read!
